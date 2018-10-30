14b5d0b1414281b546740947cc2261b4f1ccef5b
diegosanchez <diego.sanchez@mercadolibre.com>
2018-10-30 14:47:02 -0300

    [form] Mocked response

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 
@@ -21,6 +22,35 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	router.GET("/form", func(c *gin.Context) {
+		resAsStr := `{
+			"form": {
+				"fields": {
+					"entries": [
+						{
+							"clazz": "combo",
+							"uuid": "individual",
+							"values": [ "person", "company" ]
+						}, {
+							"clazz": "text",
+							"uuid": "id_company",
+							"validation": ".*"
+						}, {
+							"clazz": "text",
+							"uuid": "id_person"
+						}
+					]
+				}
+			}
+		}`
+
+		var res gin.H
+
+		json.Unmarshal([]byte(resAsStr), &res)
+
+		c.JSON(200, res)
 	})
 
 	router.Run(":" + port)
